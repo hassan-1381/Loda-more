222d9e6512c95855e9ec4c6ef1ec28e0e279e61e
hasan <hassanagabalazada81tr@gmail.com>
2024-06-10 23:29:17 +0330

    w0pwe

---
+++ script/app.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector(".btn")
+const btnText = document.querySelector(".btn__text")
+const text = document.querySelector(".text")
+
+
+btn.addEventListener("click", (event) => {
+
+    text.classList.toggle("openbox")
+
+
+})
+
+
+
